f7002c9dc8b93aa9d25ff43336e9dd57e3860cde
Samaze123 <sam.f.lambert@gmail.com>
2023-04-18 11:39:10 +0200

    web working

--- user/install_postfix.sh
+++ user/install_postfix.sh
@@ -4,6 +4,7 @@ export DEBIAN_FRONTEND=noninteractive
 # Install Postfix with the "Internet Site" configuration
 debconf-set-selections <<< "postfix postfix/mailname string woodytoys.test"
 debconf-set-selections <<< "postfix postfix/main_mailer_type select Internet Site"
+apt-get update
 apt-get install -y postfix
 
 # Update main.cf with your domain
